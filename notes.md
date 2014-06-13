e8c5a56360da6af488a3b78c020c370c8ca6706a
mohsenil85 <mohsenil85@gmail.com>
2014-06-12 20:54:15 -0700

    v2

--- notes.md
+++ notes.md
@@ -2,3 +2,5 @@ v2 notes
 1.  edit index.html to give it the correct structure -- eventually
 this will all be pulled out into templates
 2.  edit style.css to give the correct look and feel.  side by side buttons
+    2.5 (animations will be done with jq cause css is too hard)
+3.  add our main jq script to make the page function
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,47 @@
 "use strict";
 $(document).ready( function(){
-  alert('jq is working');
+
+  //globals
+  var player1Score = 0;
+  var player2Score = 0;
+
+  //init score
+  $('.player1-score').html(player1Score);
+  $('.player2-score').html(player2Score);
+
+  //create function to increment scores
+  //note we have to have a way to link the click of the
+  //button to the correct score...
+  //could make this more generic, but for now will just have 
+  //one call back per button, ie, 2 functions that essentially
+  //do the same thing
+  //
+  //also note the copy-pasted line from above.  perhaps refactor?
+
+  $('#player1-button').click(function(){
+    player1Score++;
+    $('.player1-score').html(player1Score);
+  })
+
+  //copypaste and change all occurances of '1' to '2'
+  $('#player2-button').click(function(){
+    player2Score++;
+    $('.player2-score').html(player2Score);
+  })
+
+  //attatch some hover state listeners to the buttons to make them pretty
+  //
+  $('button')
+  .mouseover(function(){
+    $(this).animate({
+      backgroundColor:"#dfdfdf"
+    }, '100')
+  })
+  .mouseout(function(){
+    $(this).animate({
+      backgroundColor:"#ffffff"
+    }, '100')
+
+  })
+
 });
